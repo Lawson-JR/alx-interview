51ce68337606457bcead57e8ad5d1d0de79748f4
Lawson-JR <140719079+Lawson-JR@users.noreply.github.com>
2024-08-01 19:09:47 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,9 +1,13 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
+"""
+Solution to the nqueens problem
+"""
 import sys
 
 def is_valid(board, row, col):
-    # Check if a queen can be placed at board[row][col]
+    """
+    Check if a queen can be placed at board[row][col]
+    """
     for i in range(row):
         if board[i] == col or \
            board[i] - i == col - row or \
@@ -14,27 +18,26 @@ def is_valid(board, row, col):
 def solve_nqueens(n):
     def backtrack(row):
         if row == n:
-            solutions.append(board[:])
+            res = []
+            for i in range(n):
+                res.append([i, board[i]])
+            print(res)
             return
+        
         for col in range(n):
             if is_valid(board, row, col):
                 board[row] = col
                 backtrack(row + 1)
+                board[row] = -1
 
-    solutions = []
     board = [-1] * n
     backtrack(0)
-    return solutions
-
-def print_solutions(solutions, n):
-    for solution in solutions:
-        print([[i, solution[i]] for i in range(n)])
 
 def main():
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
+
     try:
         n = int(sys.argv[1])
     except ValueError:
@@ -45,8 +48,7 @@ def main():
         print("N must be at least 4")
         sys.exit(1)
 
-    solutions = solve_nqueens(n)
-    print_solutions(solutions, n)
+    solve_nqueens(n)
 
 if __name__ == "__main__":
     main()
